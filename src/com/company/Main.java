12ab28cf8b25a7b41ab8df4d763a7e03d0cc5ce2
89081314528 <89081314528@mail.ru>
2019-11-24 18:45:27 +0300

    24/11

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,14 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/**1) написать метод, который принимает поле(лист) и игрока(х или 0) и проверяет, победил ли игрок
+/**
+ * 1) написать метод, который принимает поле(лист) и игрока(х или 0) и проверяет, победил ли игрок
  * 2) написать метод, который принимает поле, игрока, и номер ячейки. если игрок может поставить туда символ, то ставим
  * символ в поле и вернуть из метода  инт 0
  * если такой ячейки нет, вернуть минус 1
  * если нельзя поставить символ в ячейку, вернуть минус 2
- * ?????????? написала этот метод и добавила пока только в ход игрока х, но сильно красивее код выглядеть не стал
  * 3) предлагать игроку сделать ход 5 раз, то есть возможность ошибиться 5 раз, потом игра заканчивается победой другого
  * игрока
- * 4) ???????? почему после выигрыша игра продолжается, хотя там есть ретерн?
  * 5) дописала ничью
  */
 
@@ -30,32 +29,27 @@ public class Main {
             int numberOfCell = inputNumberOfCell.nextInt();
             int countWrongInput = 0;
             for (int g = 0; g < 5; g++) {
-                if (fillCell(field, "x ", numberOfCell) == -1) {
+                int fillCellResult = fillCell(field,"x ", numberOfCell);
+                if (fillCellResult == -1) {
                     System.out.println("Можно вносить только числа от 1 до 9");
-                    countWrongInput = countWrongInput + 1;
-                    System.out.println("Неверных попыток ввода данных " + countWrongInput);
-                    if (countWrongInput == 5) {
-                        System.out.println("Вы превысили лимит попыток ввода, победил игрок 0");
-                        return;
-                    }
-                    numberOfCell = inputNumberOfCell.nextInt();
-                } else if (fillCell(field, "x ", numberOfCell) == -2) {
+                } else if (fillCellResult == -2) {
                     System.out.println("Эта ячейка уже занята, выберите другую ячейку");
-                    countWrongInput = countWrongInput + 1;
-                    System.out.println("Неверных попыток ввода данных " + countWrongInput);
-                    if (countWrongInput == 5) {
-                        System.out.println("Вы превысили лимит попыток ввода, победил игрок 0");
-                        return;
-                    }
-                    numberOfCell = inputNumberOfCell.nextInt();
-                } else
-                   if (fillCell(field, "x ", numberOfCell) == 0) {
-                       field.set((numberOfCell - 1), "x ");
-                       break;
-                   }
+                } else if (fillCellResult == 0) {
+                    break;
+                }
+                countWrongInput = countWrongInput + 1;
+                System.out.println("Неверных попыток ввода данных " + countWrongInput);
+                if (countWrongInput == 5) {
+                    System.out.println("Вы превысили лимит попыток ввода, победил игрок 0");
+                    return;
+                }
+                numberOfCell = inputNumberOfCell.nextInt();
             }
             printField(field);
-            isPlayerWin(field, "x");
+            int isPlayerWinResult = isPlayerWin(field, "x ");
+            if (isPlayerWinResult == 1) {
+                return;
+            }
             if (i == 4) {
                 break;
             }
@@ -63,30 +57,27 @@ public class Main {
             numberOfCell = inputNumberOfCell.nextInt();
             countWrongInput = 0;
             for (int g = 0; g < 5; g++) {
-                if (numberOfCell > 9 || numberOfCell < 1) {
+                int fillCellResult = fillCell(field,"0 ", numberOfCell);
+                if (fillCellResult == -1) {
                     System.out.println("Можно вносить только числа от 1 до 9");
-                    countWrongInput = countWrongInput + 1;
-                    System.out.println("Неверных попыток ввода данных " + countWrongInput);
-                    if (countWrongInput == 5) {
-                        System.out.println("Вы превысили лимит попыток ввода, победил игрок 0");
-                        return;
-                    }
-                    numberOfCell = inputNumberOfCell.nextInt();
-                } else if (field.get(numberOfCell - 1).equals("0 ") || field.get(numberOfCell - 1).equals("x ")) {
+                } else if (fillCellResult == -2) {
                     System.out.println("Эта ячейка уже занята, выберите другую ячейку");
-                    countWrongInput = countWrongInput + 1;
-                    System.out.println("Неверных попыток ввода данных " + countWrongInput);
-                    if (countWrongInput == 5) {
-                        System.out.println("Вы превысили лимит попыток ввода, победил игрок x");
-                        return;
-                    }
-                    numberOfCell = inputNumberOfCell.nextInt();
-                } else
+                } else if (fillCellResult == 0) {
                     break;
+                }
+                countWrongInput = countWrongInput + 1;
+                System.out.println("Неверных попыток ввода данных " + countWrongInput);
+                if (countWrongInput == 5) {
+                    System.out.println("Вы превысили лимит попыток ввода, победил игрок 0");
+                    return;
+                }
+                numberOfCell = inputNumberOfCell.nextInt();
             }
-            field.set((numberOfCell - 1), "0 ");
             printField(field);
-            isPlayerWin(field, "0");
+            isPlayerWinResult = isPlayerWin(field, "0 ");
+            if (isPlayerWinResult == 1) {
+                return;
+            }
         }
         System.out.println("Ничья");
     }
@@ -106,29 +97,19 @@ public class Main {
         System.out.println();
     }
 
-    public static void isPlayerWin(List<String> field, String player) {
-        if (player.equals("x") && ((field.get(0).equals("x ") && field.get(1).equals("x ") && field.get(2).equals("x "))
-                || (field.get(3).equals("x ") && field.get(4).equals("x ") && field.get(5).equals("x "))
-                || (field.get(6).equals("x ") && field.get(7).equals("x ") && field.get(8).equals("x "))
-                || (field.get(0).equals("x ") && field.get(3).equals("x ") && field.get(6).equals("x "))
-                || (field.get(1).equals("x ") && field.get(4).equals("x ") && field.get(7).equals("x "))
-                || (field.get(2).equals("x ") && field.get(5).equals("x ") && field.get(8).equals("x "))
-                || (field.get(0).equals("x ") && field.get(4).equals("x ") && field.get(8).equals("x "))
-                || (field.get(2).equals("x ") && field.get(4).equals("x ") && field.get(6).equals("x ")))) {
-            System.out.println("Выиграл игрок Х");
-            return;
+    public static int isPlayerWin(List<String> field, String player) {
+        if ((field.get(0).equals(player) && field.get(1).equals(player) && field.get(2).equals(player))
+                || (field.get(3).equals(player) && field.get(4).equals(player) && field.get(5).equals(player))
+                || (field.get(6).equals(player) && field.get(7).equals(player) && field.get(8).equals(player))
+                || (field.get(0).equals(player) && field.get(3).equals(player) && field.get(6).equals(player))
+                || (field.get(1).equals(player) && field.get(4).equals(player) && field.get(7).equals(player))
+                || (field.get(2).equals(player) && field.get(5).equals(player) && field.get(8).equals(player))
+                || (field.get(0).equals(player) && field.get(4).equals(player) && field.get(8).equals(player))
+                || (field.get(2).equals(player) && field.get(4).equals(player) && field.get(6).equals(player))) {
+            System.out.println("Выиграл игрок " + player);
+            return 1;
         }
-        if (player.equals("0") && ((field.get(0).equals("0 ") && field.get(1).equals("0 ") && field.get(2).equals("0 "))
-                || (field.get(3).equals("0 ") && field.get(4).equals("0 ") && field.get(5).equals("0 "))
-                || (field.get(6).equals("0 ") && field.get(7).equals("0 ") && field.get(8).equals("0 "))
-                || (field.get(0).equals("0 ") && field.get(3).equals("0 ") && field.get(6).equals("0 "))
-                || (field.get(1).equals("0 ") && field.get(4).equals("0 ") && field.get(7).equals("0 "))
-                || (field.get(2).equals("0 ") && field.get(5).equals("0 ") && field.get(8).equals("0 "))
-                || (field.get(0).equals("0 ") && field.get(4).equals("0 ") && field.get(8).equals("0 "))
-                || (field.get(2).equals("0 ") && field.get(4).equals("0 ") && field.get(6).equals("0 ")))) {
-            System.out.println("Выиграл игрок 0");
-            return;
-        }
+        return -1;
     }
 
     public static int fillCell(List<String> field, String player, int numberOfCell) {
@@ -137,9 +118,8 @@ public class Main {
         } else if (field.get(numberOfCell - 1).equals("0 ") || field.get(numberOfCell - 1).equals("x ")) {
             return -2;
         } else
-//            field.set((numberOfCell - 1), player);
-//        не получается заполнить ячейку
-        return 0;
+            field.set((numberOfCell - 1), player);
+            return 0;
     }
 }
 
